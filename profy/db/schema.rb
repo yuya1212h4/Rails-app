903f5b5cad98bb46d73f3fc7271a5622ae72baeb
yuyatabata <yuya1212h4.066@gmail.com>
2016-06-09 16:56:25 +0900

    ログイン機能にグループ認証追加

--- profy/db/schema.rb
+++ profy/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603223121) do
+ActiveRecord::Schema.define(version: 20160609064951) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "key",        limit: 255
+    t.text     "detail",     limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -30,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160603223121) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.integer  "group_id",               limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
