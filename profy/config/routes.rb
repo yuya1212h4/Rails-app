1e9cdef11efaae30993fd410d68d49ed7099f215
yuyatabata <yuya1212h4.066@gmail.com>
2016-06-09 17:09:48 +0900

    resoucesメソッド showアクションのみ指定

--- profy/config/routes.rb
+++ profy/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"top#index"
+  resources :users, only: [:show]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
