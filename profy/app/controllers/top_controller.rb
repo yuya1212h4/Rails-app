08786a719d9998b724dae0273f8a2e2bb527c102
yuyatabata <yuya1212h4.066@gmail.com>
2016-06-13 02:17:12 +0900

    タイムラインに回答を表示

--- profy/app/controllers/top_controller.rb
+++ profy/app/controllers/top_controller.rb
@@ -2,6 +2,7 @@ class TopController < ApplicationController
 
   def index
     @question = Question.new
-    @questions = current_user.group.questions
+    feed_contents = current_user.group.feed_contents.includes(:content)
+    @feed_contents = feed_contents.map(&:content)
   end
 end
