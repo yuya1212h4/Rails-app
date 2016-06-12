52ba944353696ebab65337c545522bc233ccc0b9
yuyatabata <yuya1212h4.066@gmail.com>
2016-06-12 21:56:26 +0900

    回答を投稿出来るようにした

--- profy/app/controllers/answers_controller.rb
+++ profy/app/controllers/answers_controller.rb
@@ -5,4 +5,12 @@ class AnswersController < ApplicationController
     @answer.question_id = @question.id
   end
 
+  def create
+    @answer = Answer.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:answer).permit(:question_id, :text).merge(user_id: current_user.id)
+  end
 end
