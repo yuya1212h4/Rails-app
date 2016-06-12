e1442e50b6d8b23a96e20a238f790ca306215cb7
yuyatabata <yuya1212h4.066@gmail.com>
2016-06-12 22:11:29 +0900

    部分テンプレートを編集 一つの質問に一度しか回答出来ないようにした

--- profy/app/controllers/answers_controller.rb
+++ profy/app/controllers/answers_controller.rb
@@ -1,4 +1,7 @@
 class AnswersController < ApplicationController
+
+  before_action :redirect, only: :new
+
   def new
     @question = Question.find(params[:question_id])
     @answer = Answer.new
@@ -13,4 +16,11 @@ class AnswersController < ApplicationController
   def create_params
     params.require(:answer).permit(:question_id, :text).merge(user_id: current_user.id)
   end
+
+  def redirect
+    if Answer.exists?(question_id: params[:question_id], user_id: current_user.id)
+      redirect_to :root
+    end
+  end
+
 end
