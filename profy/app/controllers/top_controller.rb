7fea3e9f74dcfb77d25acb1684647416d7a81571
yuyatabata <yuya1212h4.066@gmail.com>
2016-06-13 05:50:24 +0900

    ページネーション実装

--- profy/app/controllers/top_controller.rb
+++ profy/app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     @question = Question.new
-    feed_contents = current_user.group.feed_contents.includes(:content)
-    @feed_contents = feed_contents.map(&:content)
+    @feed_contents = current_user.group.feed_contents.includes(:content).page(params[:page]).per(5)
+    @feed_contents_resource = @feed_contents.map(&:content)
   end
 end
