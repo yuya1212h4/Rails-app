74bf58c87acd9d6197a9eacbf79b78e3911e83de
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-15 19:41:36 +0900

    ルートパスの設定

--- pictweet2/config/routes.rb
+++ pictweet2/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
+  root 'tweets#index'
+  #get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
 end
