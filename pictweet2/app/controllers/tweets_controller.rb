4ffb5da1d88a53814569bc6e41335d078bd19a05
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-15 21:52:29 +0900

    サインアップまで

--- pictweet2/app/controllers/tweets_controller.rb
+++ pictweet2/app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+
+  before_action :move_to_index, expect: :index
   def index
     @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -14,4 +16,8 @@ class TweetsController < ApplicationController
   def tweet_params
     params.permit(:name, :image, :text)
   end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+    end
 end
