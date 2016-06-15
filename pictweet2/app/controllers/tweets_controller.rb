eee6a9d0452e94b87067a77d32cd7155b23865bb
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-15 19:18:58 +0900

    ビューで入力した値の受け取り

--- pictweet2/app/controllers/tweets_controller.rb
+++ pictweet2/app/controllers/tweets_controller.rb
@@ -2,4 +2,16 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+  end
+
+  def create
+      Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
 end
