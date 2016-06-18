fb715662bd63fd168fbc65aa9a776b904dd74481
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-18 14:53:04 +0900

    マイページの作成

--- pictweet2/config/routes.rb
+++ pictweet2/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  get '/tweets' => 'tweets#index'
-  get '/tweets/new' => 'tweets#new'
-  post  '/tweets'      =>  'tweets#create'    #ツイート投稿機能
+  get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  get 'users/:id' => 'users#show'
 end
