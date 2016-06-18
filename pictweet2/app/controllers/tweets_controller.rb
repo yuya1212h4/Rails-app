4611a8a24130c75997d8038a336b7eabff50542f
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-18 14:22:43 +0900

    ユーザーidの登録

--- pictweet2/app/controllers/tweets_controller.rb
+++ pictweet2/app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
-  before_action :move_to_index, expect: :index
+  before_action :move_to_index, except: :index
+
   def index
     @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -9,7 +10,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-      Tweet.create(tweet_params)
+      Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text],user_id: current_user.id)
   end
 
   private
@@ -19,5 +20,5 @@ class TweetsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
-    end
+  end
 end
--- pictweet2/config/routes.rb
+++ pictweet2/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  get '/tweets' => 'tweets#index'
+  get '/tweets/new' => 'tweets#new'
+  post  '/tweets'      =>  'tweets#create'    #ツイート投稿機能
 end
