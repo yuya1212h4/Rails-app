0cf66e35807160acdb96daa7f41b298a33f34512
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-18 16:49:56 +0900

    ツイート一覧画面にニックネームの表示

--- pictweet2/db/schema.rb
+++ pictweet2/db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615224709) do
+ActiveRecord::Schema.define(version: 20160618074755) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "name",       limit: 255
     t.text     "text",       limit: 65535
     t.text     "image",      limit: 65535
     t.datetime "created_at"
