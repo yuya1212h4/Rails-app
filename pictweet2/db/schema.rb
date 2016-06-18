3ea9dce264afffe38c946515e2497754d3b44b3a
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-18 14:23:15 +0900

    ユーザーidのカラムの追加

--- pictweet2/db/schema.rb
+++ pictweet2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615121539) do
+ActiveRecord::Schema.define(version: 20160615224709) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160615121539) do
     t.text     "image",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
