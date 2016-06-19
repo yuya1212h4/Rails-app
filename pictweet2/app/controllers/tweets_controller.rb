34396fa2137486f2c85b75ad7e21af79688070ba
田畑雄也 <tabatayuuya@tabatayuuya-no-MacBook-Pro.local>
2016-06-19 15:53:58 +0900

    削除機能の追加

--- pictweet2/app/controllers/tweets_controller.rb
+++ pictweet2/app/controllers/tweets_controller.rb
@@ -13,6 +13,13 @@ class TweetsController < ApplicationController
       Tweet.create(image: tweet_params[:image], text: tweet_params[:text],user_id: current_user.id)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
--- pictweet2/config/routes.rb
+++ pictweet2/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'tweets/new' => 'tweets#new'
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
   get 'users/:id' => 'users#show'
+  delete 'tweets/:id' => 'tweets#destroy'
 end
